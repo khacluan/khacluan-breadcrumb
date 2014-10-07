0bb54d3d6a463935d4d8144d5230b2b873ec1a73
khacluan <dangluan20@gmail.com>
2014-10-07 13:31:50 +0700

    update new code

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "breadcrumb for your meteor application",
-  version: "0.0.6",
+  version: "0.0.8",
   git: "git@github.com:khacluan/khacluan-breadcrumb.git"
 });
 
@@ -8,6 +8,10 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
 
   api.use([
+    'deps',
+    'templating',
+    'handlebars',
+    'less',
     "coffeescript",
   ], ["client", "server"]);
 
