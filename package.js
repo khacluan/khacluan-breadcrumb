0c7100ed0ea67e461bdbbbf3838e246355e5e3c4
khacluan <dangluan20@gmail.com>
2014-10-07 13:19:39 +0700

    update code

--- package.js
+++ package.js
@@ -1,25 +1,32 @@
 Package.describe({
   summary: "breadcrumb for your meteor application",
-  version: "0.0.4",
+  version: "0.0.5",
   git: "git@github.com:khacluan/khacluan-breadcrumb.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
+
+  api.use([
+    "standard-app-packages",
+    "templating",
+    "coffeescript",
+  ], ["client", "server"]);
+
   api.addFiles([
-    'client/khacluan:breadcrumb.coffee',
-    'client/khacluan:helpers.coffee',
+    'client/breadcrumb.coffee',
+    'client/helpers.coffee',
     'client/views/breadcrumb/crumb.coffee',
     'client/views/breadcrumb/crumb.html'
   ], 'client');
 
-  api.addFiles('server/khacluan:breadcrumb.coffee', 'server');
+  api.addFiles('server/breadcrumb.coffee', 'server');
 
   api.export('Breadcrumb', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('khacluan:breadcrumb');
+  api.use('breadcrumb');
   api.addFiles('test/breadcrumb-tests.js');
 });
